5984804c7f89d367ef547f25ea7473e7534b685c
Hellflavour <coolayush.sahu2001@gmail.com>
2024-09-24 17:00:30 +0530

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import About from "./components/About";
-import Home from "./components/Home";
-import Nav from "./components/Nav";
 
 
 const App = () => {
-  let data ="data from server";
-  let subject = ["Math"," ","English"]
-  let profile = {
-    name: "John doe",
-    age: 25,
-    email: "johndoe@doe.com",
-  }
+  let users =[
+    {
+      name: "john doe",
+      age: 35
+    },
+    {
+      name: "siddhant",
+      age: 45
+
+    }
+  ]
+  let hah = users.map((u,i)=> <h1 key={i}>{u.name}</h1> )
+ 
   return (
     <div>
-     <Home/>
-     <Nav/>
-     <About/>
-     <h1>{data}</h1>
-     <h1>{subject}</h1>
-     <h1>{profile.name}</h1>
-     <h1>{profile.age}</h1>
-     <h1>{profile.email}</h1>
 
+        {hah}
+        
     </div>
-  );
-};
+  )
+}
 
 export default App
